bc057b77d28cccfccbdfa612e32065e80f93d4f3
softtech <amols424@gmail.com>
2021-02-16 11:54:45 +0530

    Added: user choice to select

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,5 +1,7 @@
 package tiktaktoe;
 
+import java.util.Scanner;
+
 public class Tictactoe {
 	static char []arr=new char[10];
 	public  static char[] board() {
@@ -10,9 +12,27 @@ public class Tictactoe {
 		//This method create a board
 		return arr;
 	}
+	public static void mkChoice() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter x or o");
+		char player1=sc.next().charAt(0);
+		char comp=' ';
+		if(player1=='o'||player1=='O' ) {
+			comp='X';
+		}
+		else if(player1=='x'||player1=='X') {
+			comp='O';
+			}
+		else {
+			System.out.println("invalid entry");
+		}
+		System.out.println("Player is"+player1+" "+"computer is"+comp);
+	sc.close();
+	}
 
 	public static void main(String[] args) {
 			board();
+			mkChoice();
 	}
 
 }
