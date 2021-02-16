c17454e81854966a7211dadfd8c8f57637d81ab8
softtech <amols424@gmail.com>
2021-02-16 09:18:08 +0530

    Added UC 1

--- Tiktaktoe.java
+++ Tiktaktoe.java
@@ -1,9 +1,16 @@
 package tiktaktoe;
 
 public class Tiktaktoe {
+	char []arr=new char[10];
+	public void method1() {
+	for(int i=1;i<10;i++) {
+		arr[i]=' ';
+	
+	}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 
 	}
 
