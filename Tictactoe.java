7d3e7044602e13ec7cfa40f90dcb30dc09f8eac9
softtech <amols424@gmail.com>
2021-02-16 12:09:12 +0530

    Display:Board

--- Tictactoe.java
+++ Tictactoe.java
@@ -12,7 +12,7 @@ public class Tictactoe {
 		//This method create a board
 		return arr;
 	}
-	public static void mkChoice() {
+	public static void mkChoice() {//to make user choice
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter x or o");
 		char player1=sc.next().charAt(0);
@@ -29,10 +29,19 @@ public class Tictactoe {
 		System.out.println("Player is"+player1+" "+"computer is"+comp);
 	sc.close();
 	}
-
+public static void disBoard (char[]board) {
+	System.out.println(board[1]+"|"+board[2]+"|"+board[3]);
+	System.out.println("-------");
+	System.out.println(board[4]+"|"+board[5]+"|"+board[6]);
+	System.out.println("-------");
+	System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
+	System.out.println(board);
+	
+}
 	public static void main(String[] args) {
 			board();
 			mkChoice();
+			disBoard(board());
 	}
 
 }
